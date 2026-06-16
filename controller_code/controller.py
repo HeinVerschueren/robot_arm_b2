4085a2e8ac59fcc40db4cd392f33662a1af1261c
Hein <Hbl.Verschueren@student.avans.nl>
2026-06-16 12:56:25 +0200

    niet helemaal werkend

--- controller_code/controller.py
+++ controller_code/controller.py
@@ -10,56 +10,56 @@ class Controller(Node):
         super().__init__('Controller')
         self.tekst = None
 
-        self.start_stop_subsriber=self.create_subsriber(  #maakt aan 
+        self.start_stop_subsriber=self.create_subscription(  #maakt aan 
             Bool,   #type bericht lezen
             "/start",   #welk ding gesubscibt
             self.start_stop_callback,   #haalt data op
             10    #backlog aan msg
         )
 
-        self.autmote_subsriber=self.create_subsriber(  #maakt aan 
+        self.autmote_subsriber=self.create_subscription(  #maakt aan 
             Bool,   #type bericht lezen
             "/automate",   #welk ding gesubscibt
             self.automate_callback,   #haalt data op
             10    #backlog aan msg
         )
 
-        self.reset_subsriber=self.create_subsriber(  #maakt aan 
+        self.reset_subsriber=self.create_subscription(  #maakt aan 
             Bool,   #type bericht lezen
             "/reset",   #welk ding gesubscibt
             self.reset_callback,   #haalt data op
             10    #backlog aan msg
         )
 
-        self.shut_down_subsriber=self.create_subsriber(  #maakt aan 
+        self.shut_down_subsriber=self.create_subscription(  #maakt aan 
             Bool,   #type bericht lezen
             "/shut_down",   #welk ding gesubscibt
             self.shut_down_callback,   #haalt data op
             10    #backlog aan msg
         )       
 
-        self.voice_on_subsriber=self.create_subsriber(  #maakt aan 
+        self.voice_on_subsriber=self.create_subscription(  #maakt aan 
             Bool,   #type bericht lezen
             "/voice_on",   #welk ding gesubscibt
             self.voice_on_callback,   #haalt data op
             10    #backlog aan msg
         )
 
-        self.manual_override_subsriber=self.create_subsriber(  #maakt aan 
+        self.manual_override_subsriber=self.create_subscription(  #maakt aan 
             Bool,   #type bericht lezen
             "/manual_overide",   #welk ding gesubscibt
             self.manual_overide_callback,   #haalt data op
             10    #backlog aan msg
         )
 
-        self.snelheid_subsriber=self.create_subsriber(  #maakt aan 
+        self.snelheid_subsriber=self.create_subscription(  #maakt aan 
             Float32,   #type bericht lezen
             "/snelheid",   #welk ding gesubscibt
             self.snelheid_callback,   #haalt data op
             10    #backlog aan msg
         )
 
-        self.threshold_subsriber=self.create_subsriber(  #maakt aan 
+        self.threshold_subsriber=self.create_subscription(  #maakt aan 
             Float32,   #type bericht lezen
             "/threshold",   #welk ding gesubscibt
             self.threshold_callback,   #haalt data op
@@ -90,4 +90,12 @@ class Controller(Node):
 
     def manual_overide_callback(self, msg):
         self.tekst = msg.data
+
+def main():
+    rclpy.init()
+    node = Controller()
+    rclpy.spin(node)
+
+if __name__ == "__main__":
+    main()
     
